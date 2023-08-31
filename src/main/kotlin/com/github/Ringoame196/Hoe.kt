b56e8ee99673f433d7da3b9a67cb8fb42c20bb98
りんご飴196 <kazumac41@gmail.com>
2023-08-31 16:21:33 +0900

    ゾンビ召喚の杖 システム完全完成(オフハンドアイテム削除)

--- src/main/kotlin/com/github/Ringoame196/Hoe.kt
+++ src/main/kotlin/com/github/Ringoame196/Hoe.kt
@@ -4,6 +4,7 @@ import com.github.Ringoame196.Entity.Zombie
 import com.github.Ringoame196.data.Data
 import com.github.Ringoame196.data.GET
 import org.bukkit.ChatColor
+import org.bukkit.GameMode
 import org.bukkit.Material
 import org.bukkit.Sound
 import org.bukkit.entity.Player
@@ -17,21 +18,32 @@ class Hoe {
         val action = e.action
         e.isCancelled = true
         if (action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK) {
+            val offHandItem = player.inventory.itemInOffHand.itemMeta
+            if (offHandItem == null || !offHandItem.displayName.contains("[ゾンビ召喚]")) {
+                Player().errormessage("オフハンドにゾンビを持ってください", player)
+                return
+            }
             if (player.location.subtract(0.0, 1.0, 0.0).block.type != Material.GLASS && Data.DataManager.gameData.playMap == "map1") {
                 Player().errormessage("${ChatColor.RED}ガラスの上で実行してください(ガラスの上に行くには、自陣の後ろにあるボタンをクリック)", player)
                 return
             }
 
             val zombie = player.inventory.itemInOffHand
-
             Zombie().summonSorting(player, zombie)
             val teamName = GET().opposingTeamname(GET().teamName(player).toString())
             if (Data.DataManager.teamDataMap[teamName]?.zombieNotification == true) {
                 Team().sound(Sound.ENTITY_ZOMBIE_AMBIENT, teamName!!)
             }
-
+            removeOffHandItem(player)
             durable(player)
         }
+    }
+    fun removeOffHandItem(player: Player) {
+        if (player.gameMode == GameMode.CREATIVE) { return }
+        val itemInOffHand = player.inventory.itemInOffHand
+        val oneItem = itemInOffHand.clone()
+        oneItem.amount -= 1
+        player.inventory.setItemInOffHand(oneItem)
     }
     fun durable(player: Player) {
         val itemInHand = player.inventory.itemInMainHand
