c48ac504d1aed9f8c7f6a8046d8d4b6801608cf3
りんご飴196 <kazumac41@gmail.com>
2023-08-31 23:02:11 +0900

    TeamData opning削除

--- src/main/kotlin/com/github/Ringoame196/data/TeamData.kt
+++ src/main/kotlin/com/github/Ringoame196/data/TeamData.kt
@@ -8,6 +8,5 @@ import org.bukkit.inventory.Inventory
 data class TeamData(
     val chest: Inventory = Bukkit.createInventory(null, 27, "${ChatColor.DARK_GREEN}チームチェスト"),
     val entities: MutableList<Mob> = mutableListOf(), // ショップを取得
-    var opening: Boolean = false,
     var zombieNotification: Boolean = false // ゾンビ通知
 )
